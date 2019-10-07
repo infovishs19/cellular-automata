5552a5e0960784c9d2e6ac702f9adbe09d89491a
chrischne <chrischne@gmail.com>
2019-10-07 19:01:36 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,72 @@
-var grid = {};
+var grid;
 var gridWidth = 15;
 var gridHeight = 10;
-
-function setup() {
+var rectWidth;
+var rectHeight;
+function setup () {
     createCanvas(640, 360);
-
+    rectWidth = width / gridWidth;
+    rectHeight = height / gridHeight;
+    grid = create2DArray(gridWidth, gridHeight);
+    console.log(grid);
     for (let i = 0; i < gridWidth; i++) {
-        grid[i] = {};
         for (let j = 0; j < gridHeight; j++) {
-             grid[i][j] = random(0,255);  
-        }    
+            grid[i][j] = 0;
+        }
     }
     noLoop();
 }
 
-function draw() {
+function draw () {
     background(255);
-    
-    var rectWidth = width/gridWidth;
-    var rectHeight = height/gridHeight;
 
+    //zweidimensionales array zeichnen
     for (let i = 0; i < gridWidth; i++) {
         for (let j = 0; j < gridHeight; j++) {
-            let x = i*rectWidth;
-            let y = j*rectHeight;
-            console.log(x,y);
+            let x = i * rectWidth;
+            let y = j * rectHeight;
+            var val = grid[i][j];
             stroke(0);
-            console.log(grid[i][j]);
-            fill(grid[i][j]);
-            rect(x,y,rectWidth,rectHeight);
-        }   
+
+            if (val == 0) {
+                fill('white');
+            }
+            else {
+                fill('black');
+            }
+
+            rect(x, y, rectWidth, rectHeight);
+        }
     }
 }
 
 // reset board when mouse is pressed
-function mousePressed() {
+function mousePressed () {
     console.log('mouse pressed');
+    let xindex = floor(mouseX/rectWidth);
+    let yindex = floor(mouseY/rectHeight);
+    console.log(xindex,yindex);
+    if(grid[xindex][yindex] == 0){
+        grid[xindex][yindex] = 1; 
+    }
+    else{
+        grid[xindex][yindex] = 0;
+    }
+    redraw();
+}
+
+function keyTyped () {
+    updateGrid();
+}
+
+function updateGrid () {
+    console.log('updateGrid');
+}
+
+function create2DArray (w, h) {
+    var arr = new Array(w);
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = new Array(h);
+    }
+    return arr;
 }
