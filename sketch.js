759d3e42eb6826c95c7e1b6c5ab49557d5b915bf
chrischne <chrischne@gmail.com>
2019-10-14 13:12:34 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,8 @@ var grid;
 var newGrid;
 
 //number of cells in the x and y direction
-var gridWidth = 200;
-var gridHeight = 160;
+var gridWidth = 40;
+var gridHeight = 30;
 
 //the width and high of a cell when its drawn as a rectangle
 var rectWidth;
@@ -23,20 +23,11 @@ function setup () {
     rectHeight = height / gridHeight;
 
     //create a two dimensional array
-    grid = new Array(gridWidth);
-    for (var i = 0; i < grid.length; i++) {
-        grid[i] = new Array(gridHeight);
-    }
-
-    //initialize the grid cells with 0
-    for (let i = 0; i < w; i++) {
-        for (let j = 0; j < h; j++) {
-            grid[i][j] = 0;
-        }
-    }
+    grid = create2DArray(gridWidth,gridHeight);
     console.log(grid);
 
     noLoop();
+    redraw();
 }
 
 function draw () {
@@ -47,15 +38,15 @@ function draw () {
         for (let j = 0; j < gridHeight; j++) {
             let x = i * rectWidth;
             let y = j * rectHeight;
+
             var cellState = grid[i][j];
-            //stroke(0);
-            noStroke();
             if (cellState == 0) {
-                fill('white');
+                fill('grey');
             }
             else if (cellState == 1) {
                 fill('black');
             }
+            stroke('white');
             rect(x, y, rectWidth, rectHeight);
         }
     }
@@ -86,12 +77,9 @@ function keyTyped () {
 
 function updateGrid () {
     //create a two dimensional array for calculating the next generation
-    //create a two dimensional array
-    var nextGen = new Array(gridWidth);
-    for (var i = 0; i < nextGen.length; i++) {
-        nextGen[i] = new Array(gridHeight);
-    }
-
+    var nextGen = create2DArray(gridWidth,gridHeight);
+    console.log('nextGen');
+    console.log(nextGen);
     //calculate the new states for each grid cell
     //omit border cells to keep code simple
     for(var i=1; i<gridWidth-1; i++){
@@ -105,84 +93,146 @@ function updateGrid () {
             var bottomLeft = grid[i-1][j+1];
             var bottom = grid[i][j+1];
             var bottomRight = grid[i+1][j+1];
+
+            //the value of the current cell
+            let self = grid[i][j];
+
+            nextGen[i][j] = rules(self,topLeft,top,topRight,left,right,bottomLeft,bottom,bottomRight);
         }
     }
 
+    //copy the new generation to the actual grid
+    grid = nextGen;
+
 }
 
-function updateCell (xindex, yindex) {
+//diese function müsst ihr so verändern, dass das gewünschte verhalten daraus resultiert
+function rules(self,topLeft,top,topRight,left,right,bottomLeft,bottom,bottomRight){
+    //here you can set the rules which should apply to an individual cell
 
-    if (grid[xindex][yindex] == 0) {
-        return;
-    }
-    // if(grid[xindex][yindex] == 1){
-    //     newGrid[xindex][yindex] == 1;
+
+    //example rule
+    //e.g. if there are equal or more than three neighbor cells with state 1, then return 1
+    //else return 0
+    // let sum = topLeft+top+topRight+left+right+bottomLeft+bottom+bottomRight;
+    // if(sum>=3){
+    //     return 1;
     // }
-    //here we know that grid[xindex][yindex] == 1
-    for (var i = -1; i <= 1; i++) {
-        for (var j = -1; j <= 1; j++) {
-            var x = xindex + i;
-            var y = yindex + j;
-            if (x < 0 || x >= gridWidth || y < 0 || y >= gridHeight) {
-                continue;
-            }
-            //rules
-            //upper left
-            if (i == -1 && j == -1) {
-                if (random(0, 1) > 0) {
-                    newGrid[x][y] = 1;
-                }
-            }
-            //upper
-            if (i == 0 && j == -1) {
-                if (random(0, 1) > 1) {
-                    newGrid[x][y] = 1;
-                }
-            }
-            //upper right
-            if (i == 1 && j == -1) {
-                if (random(0, 1) > 0) {
-                    newGrid[x][y] = 1;
-                }
-            }
-            //left
-            if (i == -1 && j == 0) {
-                if (random(0, 1) > 1) {
-                    newGrid[x][y] = 1;
-                }
-            }
-            //center
-            if (i == 0 && j == 0) {
+    // else return 0;
 
-                newGrid[x][y] = 1;
+    //another example rule
+    //if at least one neighbor has state one, then return 1, else return 0
+    // let sum = topLeft+top+topRight+left+right+bottomLeft+bottom+bottomRight;
+    // if(sum>=1){
+    //     return 1;
+    // }
+    // else return 0;
 
-            }
-            //right
-            if (i == 1 && j == 0) {
-                if (random(0, 1) > 1) {
-                    newGrid[x][y] = 1;
-                }
-            }
-            //lower left
-            if (i == -1 && j == 1) {
-                if (random(0, 1) > 0) {
-                    newGrid[x][y] = 1;
-                }
-            }
-            //lower
-            if (i == 0 && j == 1) {
-                if (random(0, 1) > 1) {
-                    newGrid[x][y] = 1;
-                }
-            }
-            //lower right
-            if (i == 1 && j == 1) {
-                if (random(0, 1) > 0.9) {
-                    newGrid[x][y] = 1;
-                }
-            }
+    //another example rule
+    //if there is a neighbour to the direct top, left, right or bottom side,
+    //then return 1 with a certain probability, otherwise return 0 
+    if(top == 1 || right == 1  || left == 1 || bottom == 1){
+        return random(0,1) > 0.5;
+    }
+    else return 0;
 
+    //another example rule
+    // if(topLeft == 1 && topRight == 1 && bottomLeft == 1 && bottomRight == 1){
+    //     return 1;
+    // }
+    // else {
+    //     return 0;
+    // }
+}
+
+function create2DArray(w,h){
+   var arr =  new Array(w);
+    for (var i = 0; i < w; i++) {
+        arr[i] = new Array(h);
+    }
+
+    //initialize the grid cells with 0
+    for (let i = 0; i < w; i++) {
+        for (let j = 0; j < h; j++) {
+            arr[i][j] = 0;
         }
     }
+    return arr;
 }
+
+// function updateCell (xindex, yindex) {
+
+//     if (grid[xindex][yindex] == 0) {
+//         return;
+//     }
+//     // if(grid[xindex][yindex] == 1){
+//     //     newGrid[xindex][yindex] == 1;
+//     // }
+//     //here we know that grid[xindex][yindex] == 1
+//     for (var i = -1; i <= 1; i++) {
+//         for (var j = -1; j <= 1; j++) {
+//             var x = xindex + i;
+//             var y = yindex + j;
+//             if (x < 0 || x >= gridWidth || y < 0 || y >= gridHeight) {
+//                 continue;
+//             }
+//             //rules
+//             //upper left
+//             if (i == -1 && j == -1) {
+//                 if (random(0, 1) > 0) {
+//                     newGrid[x][y] = 1;
+//                 }
+//             }
+//             //upper
+//             if (i == 0 && j == -1) {
+//                 if (random(0, 1) > 1) {
+//                     newGrid[x][y] = 1;
+//                 }
+//             }
+//             //upper right
+//             if (i == 1 && j == -1) {
+//                 if (random(0, 1) > 0) {
+//                     newGrid[x][y] = 1;
+//                 }
+//             }
+//             //left
+//             if (i == -1 && j == 0) {
+//                 if (random(0, 1) > 1) {
+//                     newGrid[x][y] = 1;
+//                 }
+//             }
+//             //center
+//             if (i == 0 && j == 0) {
+
+//                 newGrid[x][y] = 1;
+
+//             }
+//             //right
+//             if (i == 1 && j == 0) {
+//                 if (random(0, 1) > 1) {
+//                     newGrid[x][y] = 1;
+//                 }
+//             }
+//             //lower left
+//             if (i == -1 && j == 1) {
+//                 if (random(0, 1) > 0) {
+//                     newGrid[x][y] = 1;
+//                 }
+//             }
+//             //lower
+//             if (i == 0 && j == 1) {
+//                 if (random(0, 1) > 1) {
+//                     newGrid[x][y] = 1;
+//                 }
+//             }
+//             //lower right
+//             if (i == 1 && j == 1) {
+//                 if (random(0, 1) > 0.9) {
+//                     newGrid[x][y] = 1;
+//                 }
+//             }
+
+//         }
+//     }
+// }
 
