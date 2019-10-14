c75e837a595737ec0390b50c28755807e36a0213
chrischne <chrischne@gmail.com>
2019-10-14 12:43:51 +0200

    getting there with shiffman style

--- sketch.js
+++ sketch.js
@@ -1,14 +1,39 @@
+
+//a two dimensional array to hold the states of all cells
 var grid;
+
+//a grid representing the next generation
 var newGrid;
+
+//number of cells in the x and y direction
 var gridWidth = 200;
 var gridHeight = 160;
+
+//the width and high of a cell when its drawn as a rectangle
 var rectWidth;
 var rectHeight;
+
+//ignore first and last cells
+//make ruleset function
 function setup () {
     createCanvas(800, 600);
+
+    //calculate the width and height of rectangles based on the canvas width and height
     rectWidth = width / gridWidth;
     rectHeight = height / gridHeight;
-    grid = create2DArray(gridWidth, gridHeight);
+
+    //create a two dimensional array
+    grid = new Array(gridWidth);
+    for (var i = 0; i < grid.length; i++) {
+        grid[i] = new Array(gridHeight);
+    }
+
+    //initialize the grid cells with 0
+    for (let i = 0; i < w; i++) {
+        for (let j = 0; j < h; j++) {
+            grid[i][j] = 0;
+        }
+    }
     console.log(grid);
 
     noLoop();
@@ -17,32 +42,33 @@ function setup () {
 function draw () {
     background(255);
 
-    //zweidimensionales array zeichnen
+    //draw the the grid 
     for (let i = 0; i < gridWidth; i++) {
         for (let j = 0; j < gridHeight; j++) {
             let x = i * rectWidth;
             let y = j * rectHeight;
-            var val = grid[i][j];
+            var cellState = grid[i][j];
             //stroke(0);
             noStroke();
-            if (val == 0) {
+            if (cellState == 0) {
                 fill('white');
             }
-            else {
+            else if (cellState == 1) {
                 fill('black');
             }
-
             rect(x, y, rectWidth, rectHeight);
         }
     }
 }
 
-// reset board when mouse is pressed
+// mouse press to change the value of a grid cell
 function mousePressed () {
     console.log('mouse pressed');
     let xindex = floor(mouseX / rectWidth);
     let yindex = floor(mouseY / rectHeight);
+
     console.log(xindex, yindex);
+
     if (grid[xindex][yindex] == 0) {
         grid[xindex][yindex] = 1;
     }
@@ -52,21 +78,36 @@ function mousePressed () {
     redraw();
 }
 
+//calculate the next generation of the grid when a key is typed
 function keyTyped () {
     updateGrid();
     redraw();
 }
 
 function updateGrid () {
-    newGrid = create2DArray(gridWidth, gridHeight);
+    //create a two dimensional array for calculating the next generation
+    //create a two dimensional array
+    var nextGen = new Array(gridWidth);
+    for (var i = 0; i < nextGen.length; i++) {
+        nextGen[i] = new Array(gridHeight);
+    }
 
-    console.log('updateGrid');
-    for (let i = 0; i < gridWidth; i++) {
-        for (let j = 0; j < gridHeight; j++) {
-            updateCell(i, j);
+    //calculate the new states for each grid cell
+    //omit border cells to keep code simple
+    for(var i=1; i<gridWidth-1; i++){
+        for(var j=1; j<gridHeight-1; j++){
+            //get the states of the neigbouring cells
+            var topLeft = grid[i-1][j-1];
+            var top = grid[i][j-1];
+            var topRight = grid[i+1][j-1];
+            var left = grid[i-1][j];
+            var right = grid[i+1][j];
+            var bottomLeft = grid[i-1][j+1];
+            var bottom = grid[i][j+1];
+            var bottomRight = grid[i+1][j+1];
         }
     }
-    grid = newGrid;
+
 }
 
 function updateCell (xindex, yindex) {
@@ -112,9 +153,9 @@ function updateCell (xindex, yindex) {
             }
             //center
             if (i == 0 && j == 0) {
-                
-                    newGrid[x][y] = 1;
-                
+
+                newGrid[x][y] = 1;
+
             }
             //right
             if (i == 1 && j == 0) {
@@ -145,15 +186,3 @@ function updateCell (xindex, yindex) {
     }
 }
 
-function create2DArray (w, h) {
-    var arr = new Array(w);
-    for (var i = 0; i < arr.length; i++) {
-        arr[i] = new Array(h);
-    }
-    for (let i = 0; i < w; i++) {
-        for (let j = 0; j < h; j++) {
-            arr[i][j] = 0;
-        }
-    }
-    return arr;
-}
